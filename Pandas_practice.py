3e7ba1442f58e4621f5282ea74a6e5e72220b427
Yogesh Lohar <yogeshlohar129@gmail.com>
2025-08-06 16:14:52 +0530

    update file

--- Pandas_practice.py
+++ Pandas_practice.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-data = pd.read_csv("Book1.csv")
-print(data)
+data = pd.read_csv(r".\EV_projects\Book1.csv")
+
+s1 = data.loc[data["Name"] == "Divya"]
+# print(s1)
+
+data2 = pd.read_excel(r".\EV_projects\netflix_titles.xlsx")
+print(data2.head())
